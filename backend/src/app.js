e6138795bf4bedc1d7c16ed5159443d1acc6ac56
NeonDreamsUnleashed <168414230+NeonDreamsUnleashed@users.noreply.github.com>
2026-05-11 23:22:51 +0300

    add road

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-import taskRoutes from './routes/taskRoutes.js';
 
+import taskRoutes from './routes/taskRoutes.js';
 import authRoutes from './routes/authRoutes.js';
 
 const app = express();
@@ -10,6 +10,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Server is running"
+  });
+});
+
 app.use('/api/tasks', taskRoutes);
 app.use('/api/auth', authRoutes);
 
